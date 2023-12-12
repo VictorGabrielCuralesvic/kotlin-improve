988395d85160ede824823864c28eed0176398b78
Nome <miranhaomi@gmail.com>
2023-12-12 19:57:16 -0300

    Classes Genericas

---
+++ generics/generic-class.kt
@@ -0,0 +1,42 @@
+
+class MutableStack<E>(vararg items: E) {
+    
+    private val elements = items.toMutableList()
+
+    fun push(element: E) = elements.add(element)
+
+    fun peek(): E = elements.last()
+
+    fun pop(): E = elements.removeAt(elements.size - 1)
+
+    fun isEmpty() = elements.isEmpty()
+
+    fun size() = elements.size
+
+    override fun toString() = "MutableStack(${elements.joinToString()})"
+}
+
+// generic functions
+
+fun <E> mutableStackOf(vararg elements: E) = MutableStack(*elements)
+
+fun main() {
+    val stack = mutableStackOf(0.62, 3.14, 2.7)
+    println(stack)
+}
+
+/* fun main() {
+
+    val stack = MutableStack(0.62, 3.14, 2.7)
+    println(stack)
+    stack.push(9.87)
+    println(stack)
+
+    println("peek(): ${stack.peek()}")
+    println(stack)
+    
+    for (i in 1..stack.size()) {
+        println("pop(): ${stack.pop()}")
+        println(stack)
+    }
+} */
