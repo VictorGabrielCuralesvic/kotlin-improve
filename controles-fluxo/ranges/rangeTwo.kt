8b96a3f4c7fc1fe54f03cfbc1d603cf0c99ec32d
Nome <miranhaomi@gmail.com>
2023-12-12 20:02:42 -0300

    Ranges in Kotlin

---
+++ controles-fluxo/ranges/rangeTwo.kt
@@ -0,0 +1,16 @@
+
+fun main() {
+    
+    for (c in 'a'..'d') {
+        print(c)
+    }
+
+    println(" ")
+
+    for (c in 'z' downTo 's' step 2) {
+        print(c)
+    }
+
+    println(" ")
+
+}
