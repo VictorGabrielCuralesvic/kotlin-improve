9b7e2fd1dc064013eba40b457522f248910d4f5c
Nome <miranhaomi@gmail.com>
2023-12-12 20:09:29 -0300

    Sets in Kotlin

---
+++ coleções/set/set.kt
@@ -0,0 +1,20 @@
+/* não aceita nada duplicado */
+
+val openIssues: MutableSet<String> = mutableSetOf("uniqueDescr1", "uniqueDescr2", "uniqueDescr3")
+
+fun addIssue(uniqueDesc: String): Boolean {
+    return openIssues.add(uniqueDesc)
+}
+
+fun getStatusLog(isAdded: Boolean): String {
+    return if (isAdded) "register correctly." else "marked as duplicate and rejected."
+}
+
+fun main() {
+    val aNewIssue: String = "uniqueDescr4"
+    val anIssueAlreadyIn: String = "uniqueDescr2"
+
+    println("Issue $aNewIssue ${getStatusLog(addIssue(aNewIssue))}")
+    println("Issue $anIssueAlreadyIn ${getStatusLog(addIssue(anIssueAlreadyIn))}")
+    
+}
